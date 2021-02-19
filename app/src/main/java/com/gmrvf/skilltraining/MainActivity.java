e17d0223cb8fc436f5ae34f50b0c9636af1dd245
saikiran1224 <knvrssaikiran@gmail.com>
2021-02-19 10:47:56 +0530

    Added Login and SignUp Screens

--- app/src/main/java/com/gmrvf/skilltraining/MainActivity.java
+++ app/src/main/java/com/gmrvf/skilltraining/MainActivity.java
@@ -2,7 +2,12 @@ package com.gmrvf.skilltraining;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.gmrvf.skilltraining.general.LoginActivity;
+import com.gmrvf.skilltraining.general.SignUpActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        findViewById(R.id.btnSignup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
     }
 }
